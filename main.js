7b839c05a39b39abaa481940da952fc2e3f594e4
Peter Jensen <tuximail@gmail.com>
2018-09-20 15:15:17 +0200

    opening links in default browser window

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -64,6 +64,17 @@ app.on('activate', function() {
     if (mainWindow === null) {
         createWindow();
     }
+});
+
+// TODO check if its good idea!
+app.on('web-contents-created', (event, contents) => {
+    contents.on('new-window', (event, navigationUrl) => {
+        // Ask the operating system
+        // to open this event's url in the default browser.
+        event.preventDefault();
+
+        shell.openExternal(navigationUrl);
+    });
 });
 
 // In this file you can include the rest of your app's specific main process
