d24086c346c11068a1d5b54ee86a46f12a165195
Peter Jensen <tuximail@gmail.com>
2019-01-11 14:50:56 +0100

    added code quality tools

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-var isElectron = require('is-electron-renderer');
+const isElectron = require('is-electron-renderer');
 
 window.isElectron = isElectron;
 
-function loadApp() { 
+function loadApp() {
     return new Promise((resolve, reject) => {
         console.log('is this running in electron.js?: ', isElectron);
         const msgEl = document.getElementById('message');
-        
-        const runtime = isElectron ? 'electron.js' : 'a web browser'
-        
-        msgEl.innerHTML = 'This is ' + runtime + "!!!";
-        
+
+        const runtime = isElectron ? 'electron.js' : 'a web browser';
+
+        msgEl.innerHTML = `This is ${runtime}!!!`;
+
         if (isElectron) {
-            console.log('requiring fs here worked in webpack 2, but not in 4, as it seems now it breaks the web build... to be continued...')
+            console.log(
+                'requiring fs here worked in webpack 2, but not in 4, as it seems now it breaks the web build... to be continued...'
+            );
+        } else {
+            resolve();
         }
-        else {
-            resolve()
-        }
-        
-    })
+    });
 }
 
-loadApp().then(res => console.log(res), err => console.log(err))
+loadApp().then(res => console.log(res), err => console.log(err));
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,73 @@
 /* eslint strict: 0 */
+
 'use strict';
 
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const [,,command, value] = process.argv;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const [, , command, value] = process.argv;
 const srcPath = path.join(__dirname, 'src');
-const isWeb = (process.env.APP_TARGET &&  process.env.APP_TARGET === 'WEB');
+const isWeb = process.env.APP_TARGET && process.env.APP_TARGET === 'WEB';
 const outputPath = path.join(__dirname, 'build', isWeb ? 'web' : 'electron');
-const indexPath = path.join(outputPath, isWeb ? 'index.html' : 'index-electron.html');
+const indexPath = path.join(
+    outputPath,
+    isWeb ? 'index.html' : 'index-electron.html',
+);
 
-console.log({ outputPath, indexPath, isWeb })
+console.log({ outputPath, indexPath, isWeb });
 
-let options = {
-  mode: process.env.NODE_ENV,
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          // plugins: [require('@babel/plugin-proposal-object-rest-spread')]
-        }
-      },
-      exclude: [ /node_modules/ ],
-    }]
-  },
-  devServer: {
-    contentBase: outputPath,
-  },
-  output: {
-    path: outputPath,
-    filename: 'bundle.js',
-  },
-  entry: './src/index',
-  plugins: [new HtmlWebpackPlugin({ 
-    title: `My ${isWeb ? 'Web' : 'Electron'} App`,
-    template: path.join(srcPath, 'index.html'),
-    filename: indexPath,
-  })]
+const options = {
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        // plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+                    },
+                },
+                exclude: [/node_modules/],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader', // compiles Sass to CSS, using Node Sass by default
+                ],
+                exclude: [/node_modules/],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: outputPath,
+    },
+    output: {
+        path: outputPath,
+        filename: 'bundle.js',
+    },
+    entry: './src/index',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: `My ${isWeb ? 'Web' : 'Electron'} App`,
+            template: path.join(srcPath, 'index.html'),
+            filename: indexPath,
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
 };
 
-options.target = isWeb ? 'web' : 'electron-renderer' 
+options.target = isWeb ? 'web' : 'electron-renderer';
 
 module.exports = options;
